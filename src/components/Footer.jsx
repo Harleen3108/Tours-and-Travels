aae68008aecd95cce686519bbfc735892e7ea5f9
Chinmmaayyy <chinmaysinghal10@gmail.com>
2026-02-27 23:54:39 +0530

    Updated About section with Indian customer images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -42,7 +42,7 @@ const Footer = () => {
                         <div className="footer-col">
                             <h4>Contact</h4>
                             <p>Email: info@toursandtravels.com</p>
-                            <p>Phone: +91 95606 57638</p>
+                            <p>Phone: +91 95606 57638 </p>
                         </div>
                     </div>
                 </div>
